59dcd9da87a77e612adbc77edddf21ddb37ffb99
Wessel <w.m.de.vroome@st.hanze.nl>
2024-09-12 20:49:47 +0200

    functie scandir? werkt dit?

--- Opdracht2/index.php
+++ Opdracht2/index.php
@@ -8,5 +8,11 @@ define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
-/* HIER CODE (zie de instructies op Blackboard) */
+function readFiles() {
+    scandir(FILES_PATH);
+ 
+}
+
+readFiles();
+print_r($files);
 
