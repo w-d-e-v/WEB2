57c89e7511e664a0efc4e6bf0b6d52af9d2f3204
Wessel <w.m.de.vroome@st.hanze.nl>
2024-09-17 21:21:07 +0200

    Geklooi met hyperlinks maken leidt tot weinig bruikbaars, nevermind this

--- Opdracht2/index.php
+++ Opdracht2/index.php
@@ -8,27 +8,19 @@ $root = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Opdracht2' . DIRECTORY_SEPARAT
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
-/*
-function readFiles() {
-    
-    opendir(FILES_PATH);
-    foreach(scandir(FILES_PATH) as $file){
-        print_r($file);
-    }
-    closedir(FILES_PATH);
- 
-}
 
-readFiles();
-*/
 
-// W3 schools to the rescue!
+//Ik heb het W3schools voorbeeld verbasteld met een foreach loop
+//Probleem is dat . en .. (current path en bovenliggend path) ook weergegeven worden
+//Ik ga eerst kijken of ik hier makkelijk hyperlinks van kan maken
+//Maar op basis van de opdracht moet ik vermoedlelijk iets met method = POST
+//sturen richting App.php zodat die de inhoud kan gaan inlezen en sorteren 😵‍💫
 
 function readFiles(){
 if (is_dir(FILES_PATH)){
     if ($dh = opendir(FILES_PATH)){
         foreach(scandir(FILES_PATH) as $file){
-            print_r($file . "<BR />");
+            print_r('<A HREF="'. FILES_PATH . DIRECTORY_SEPARATOR . $file . '>' . $file . "<BR />");
       }
       closedir($dh);
     }
