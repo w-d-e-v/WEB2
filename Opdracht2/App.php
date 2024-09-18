7e430761ca955d6570f70da00a3ab774c8e53fad
Wessel <w.m.de.vroome@st.hanze.nl>
2024-09-18 20:07:18 +0200

    Filename extractie = oke

--- Opdracht2/App.php
+++ Opdracht2/App.php
@@ -2,6 +2,10 @@
 
 declare(strict_types = 1);
 
-//Eerst uitvogelen welk bestand we binnenkrijgen
-$fileName = $_GET;
-var_dump($fileName);
+//Eerst uitvogelen welk bestand we binnenkrijgen. Dit doen we door een vieze
+//hack want kennelijk is de filename de key en niet de value in dit GET array
+$fileArray = $_GET;
+foreach ($fileArray as $fileName=>$value){
+
+print_r($fileName);
+}
