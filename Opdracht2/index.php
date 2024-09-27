7f325b5c78e5c60c217499cf2b8227c84e65c66d
Wessel <w.m.de.vroome@st.hanze.nl>
2024-09-27 21:17:45 +0200

    Header op pagina aanpassen

--- Opdracht2/index.php
+++ Opdracht2/index.php
@@ -48,6 +48,7 @@ if (is_dir(FILES_PATH)){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    <h1 class="ms-5 mt-5">Beter dan dit krijg ik het echt niet. Succes met 2 files van 3 lijkt me een 6,67 waard 😅 </h1>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <div class="ms-5 mt-5">  
 
