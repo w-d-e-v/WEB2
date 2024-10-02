6417d1e825fb974287fefbd7ef1d9f71893a53f5
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-02 20:58:21 +0200

    shares base class toevoegen obv. wat er in users gebeurt

---
+++ Opdracht3/classes/shares.class.php
@@ -0,0 +1,16 @@
+<?php
+    class Shares extends Dbh {
+        protected function getShare($id) {
+            $sql = "SELECT * FROM shares WHERE id = ?";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute([$id]);
+            // Use fetch() for 1 row, and fetchAll() for all rows
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
+    }
+    protected function setShare($user_id, $title, $body, $link) {
+        $sql = "INSERT INTO shares(user_id, title, body, link) VALUES (?, ?, ?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$user_id, $title, $body, $link]);
+      }
+}
