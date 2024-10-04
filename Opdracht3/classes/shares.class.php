855ca3182aaec78c9fcc6ab10f5a8fc5a3bcd122
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-04 21:49:07 +0200

    Totale reset naar gisteren voor het stuk was

--- Opdracht3/classes/shares.class.php
+++ Opdracht3/classes/shares.class.php
@@ -1,15 +1,15 @@
 <?php
     class Shares extends Dbh {
-        protected function getShares() { //niet meer per individuele share
+        protected function getShare($id) {
             $sql = "SELECT shares.*, users.name FROM shares 
-                    JOIN users ON shares.user_id = users.id"; //dus de query moet anders
+                JOIN users ON shares.user_id = users.id 
+                WHERE shares.id = ?";
             $stmt = $this->connect()->prepare($sql);
-            $stmt->execute();
+            $stmt->execute([$id]);
+            // Use fetch() for 1 row, and fetchAll() for all rows
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             return $results;
-        }
-    
-    
+    }
     protected function setShare($user_id, $title, $body, $link) {
         $sql = "INSERT INTO shares(user_id, title, body, link) VALUES (?, ?, ?, ?)";
         $stmt = $this->connect()->prepare($sql);
