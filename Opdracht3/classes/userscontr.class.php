9104355ecc90ec4c8a98484c86dcc0e4048542be
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-06 21:42:11 +0200

    met hashed password!!!!

--- Opdracht3/classes/userscontr.class.php
+++ Opdracht3/classes/userscontr.class.php
@@ -4,7 +4,7 @@
       if ($_SERVER['REQUEST_METHOD'] == 'POST') {
           $name = $_POST['username'];
           $email = $_POST['email'];
-          $password = $_POST['password'];
+          $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
           $this->setUser($name, $email, $password);
       }
   }
