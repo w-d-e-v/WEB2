3099b7bae3b340b8265666b7f8e5fed0edfde976
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-06 22:04:40 +0200

    En nu kunnen we shares toevoegen!

--- Opdracht3/classes/sharescontr.class.php
+++ Opdracht3/classes/sharescontr.class.php
@@ -1,6 +1,20 @@
-<?php
+<?php /*
   class SharesContr extends Shares {
     public function createShare($user_id, $title, $body, $link) {
       $this->setShare($user_id, $title, $body, $link);
     }
   }
+
+*/
+
+  class SharesContr extends Shares {
+  public function createShare() {
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          $user_id = $_POST['user_id'];
+          $title = $_POST['title'];
+          $body = $_POST['body'];
+          $link = $_POST['link'];
+          $this->setShare($user_id, $title, $body, $link);
+      }
+  }
+}
