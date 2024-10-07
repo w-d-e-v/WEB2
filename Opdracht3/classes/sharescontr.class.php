c3d1510b58163303604b54c3e13e37e91a58444a
Wessel <w.m.de.vroome@st.hanze.nl>
2024-10-07 11:54:23 +0200

    Opkuisen oude sharescontroller code

--- Opdracht3/classes/sharescontr.class.php
+++ Opdracht3/classes/sharescontr.class.php
@@ -1,11 +1,4 @@
-<?php /*
-  class SharesContr extends Shares {
-    public function createShare($user_id, $title, $body, $link) {
-      $this->setShare($user_id, $title, $body, $link);
-    }
-  }
-
-*/
+<?php 
 
   class SharesContr extends Shares {
   public function createShare() {
